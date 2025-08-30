7299df175d8de6eb99dbf31c4c61662adfbf4f12
Ajay <ajayvishwakarma457@gmail.com>
2025-08-31 01:31:15 +0530

    changes

--- index.js
+++ index.js
@@ -17,22 +17,22 @@ app.get('/about', (req, res) => {
   res.send('About us page.');
 });
 
-app.get('/products', (req, res) => {
-  res.json({ products: ['Product 1', 'Product 2', 'Product 3'] });
-});
+// app.get('/products', (req, res) => {
+//   res.json({ products: ['Product 1', 'Product 2', 'Product 3'] });
+// });
 
 
-app.get('/service', (req, res) => {
-  res.send('Our services include web development, cloud solutions, and more.');
-});
+// app.get('/service', (req, res) => {
+//   res.send('Our services include web development, cloud solutions, and more.');
+// });
 
 // app.get('/gallery', (req, res) => {
 //   res.send('Contact us at gallery');
 // });
 
-app.get('/contact', (req, res) => {
-  res.send('Contact us at contact@example.com');
-});
+// app.get('/contact', (req, res) => {
+//   res.send('Contact us at contact@example.com');
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
