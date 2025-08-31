de7b8743d502dfd52c5e6fab885905f20341b5c4
Ajay <ajayvishwakarma457@gmail.com>
2025-08-31 11:20:06 +0530

    changes

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ app.get('/products', (req, res) => {
 });
 
 
-
-// app.get('/service', (req, res) => {
-//   res.send('Our services include web development, cloud solutions, and more.');
-// });
+app.get('/service', (req, res) => {
+  res.send('Our services include web development, cloud solutions, and more.');
+});
 
 // app.get('/gallery', (req, res) => {
 //   res.send('Contact us at gallery');
